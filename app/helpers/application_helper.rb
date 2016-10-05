498e3d0ea2794d9cccd3e31848cd10aa2f260f61
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-05 16:20:16 -0500

    add nav with login/register/home links, add session helper, correct pairing s typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if logged_in?
+  end
+
+  def mentor
+    current_user.group == "mentor"
+  end
 end
--- app/helpers/sessions_helper.rb
+++
@@ -1,9 +0,0 @@
-module SessionsHelper
-  def logged_in?
-    session[:user_id] != nil
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if logged_in?
-  end
-end
