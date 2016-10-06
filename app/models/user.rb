edb3acee31912a5c00a5db864a8d62707192bebc
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-06 16:11:59 -0500

    feedback associations methods and routes updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
 
   has_many :feedbacks
 
-  # has_many :mentor_feedbacks, through: :mentor_pairings, source: :feedbacks
-  # has_many :student_feedbacks, through: :student_pairings, source: :feedbacks
+  has_many :mentor_feedbacks, through: :mentor_pairings, source: :feedbacks
+  has_many :student_feedbacks, through: :student_pairings, source: :feedbacks
 
   has_and_belongs_to_many :topics
 
@@ -16,5 +16,37 @@ class User < ActiveRecord::Base
 
   def average(ratings)
     ratings.inject{ |sum, rating| sum + rating }.to_f / ratings.size
+  end
+
+  def mentor_feedback
+    self.mentor_pairings.map do |pairing|
+      yield(pairing)
+    end
+  end
+
+  def received_mentor_feedbacks
+    pairing.feedbacks.select { |feedback| feedback.user_id == pairing.student_id }
+  end
+
+  def given_mentor_feedbacks
+    self.mentor_pairings.each do |pairing|
+      feedback += pairing.feedbacks.select { |feedback| feedback.user_id == pairing.mentor_id }
+    end
+  end
+
+  def received_student_feedbacks
+    self.student_pairings.each do |pairing|
+      feedback += pairing.feedbacks.select { |feedback| feedback.user_id == pairing.mentor_id }
+    end
+  end
+
+  def given_student_feedbacks
+    self.student_pairings.each do |pairing|
+      feedback += pairing.feedbacks.select { |feedback| feedback.user_id == pairing.student_id }
+    end
+  end
+
+  def sort
+    self.sort { |a,b| a.pairing.start_at <=> b.pairing.start_at }
   end
 end
