bcec8a8f872125f1343243937dcfc8dcbd4d0f01
Caitlin Johnson & jakefishbain & Jeff Fichtner & Thuy <pair+caitlinlikesrobots.jakefishbain.jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-06 16:19:10 -0500

    restrict pairing lists

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,7 +1,7 @@
 class PairingsController < ApplicationController
 
   def index
-    @available_pairings =  Pairing.available_pairings
+    @available_pairings =  Pairing.available_pairings(current_user)
     @upcoming_mentor_pairings = Pairing.upcoming_mentor_pairings(current_user)
     @previous_mentor_pairings = Pairing.previous_mentor_pairings(current_user)
     @upcoming_student_pairings = Pairing.upcoming_student_pairings(current_user)
