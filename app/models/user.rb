9cce578c2254f9f2d0787121626726ff5372dd99
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-06 14:57:30 -0500

    User to feedback association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
-  # has_many :feedbacks
   has_many :mentor_pairings, class_name: "Pairing", foreign_key: :mentor_id
   has_many :student_pairings, class_name: "Pairing", foreign_key: :student_id
 
-  has_many :mentor_feedbacks, through: :mentor_pairings, source: :feedbacks
-  has_many :student_feedbacks, through: :student_pairings, source: :feedbacks
+  has_many :feedbacks
+
+  # has_many :mentor_feedbacks, through: :mentor_pairings, source: :feedbacks
+  # has_many :student_feedbacks, through: :student_pairings, source: :feedbacks
 
   has_and_belongs_to_many :topics
 
