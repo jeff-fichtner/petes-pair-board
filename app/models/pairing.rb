875fe581cc8c319f567c98a48841f97208dda474
Thuy <email": null>
2016-10-05 20:58:31 -0500

    add start_time_text virtual attribute methods in Pairing class to return formatted date regardless of input, add to strong params, change text_field to virtual attribute

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,4 +2,28 @@ class Pairing < ActiveRecord::Base
   has_many :feedbacks
   belongs_to :mentor, class_name: "User", foreign_key: :mentor_id
   belongs_to :student, class_name: "User", foreign_key: :student_id
+
+  attr_writer :start_time_text
+
+  validate :check_start_time_text
+
+  before_save :save_start_time_text
+
+  def start_time_text
+    @start_time_text || start_time.try(:strftime, "%Y-%m-%d %H:%M:%S")
+  end
+
+  def save_start_time_text
+    self.start_time = Time.zone.parse(@start_time_text) if @start_time_text.present?
+  end
+
+  # this custom validation does not work...why?
+  def check_start_time_text
+    if @start_time_text.present? && Time.zone.parse(@start_time_text).nil?
+      errors.add :start_time_text, "cannot be parsed"
+    end
+    rescue ArgumentError
+      errors.add :start_time_text, "is out of range"
+  end
+
 end
