a40f02846b924222372afd2883c0d3e2070f09f3
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-06 12:10:15 -0500

    Destroy route, login_params, login fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-    if user
-      sessions[:user_id] = user.id
+    @user = User.find_by(email: login_params[0]).try(:authenticate, login_params[1])
+    if @user
+      session[:user_id] = @user.id
       redirect_to @user
     else
-      render new_user_path(@user)
+      @user = User.new
+      @errors = ["The username or password don't match our records."]
+      render new_user_path
     end
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_user_path
+  end
+
+  private
+  def login_params
+    params.require(:user).permit(:email, :password).values
   end
 end
