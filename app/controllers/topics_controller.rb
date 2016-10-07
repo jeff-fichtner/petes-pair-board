a651b44aa6c8d3c6c89df877c7ffc4d537417ef7
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-07 14:26:40 -0500

    Topic logic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,14 @@
 class TopicsController < ApplicationController
+  before_action :set_user
+
   def create
-    p params
+    Topic.all.each do |topic|
+      if params.keys.include? (topic.topic)
+        @user.topics << topic
+      elsif @user.topics.include? (topic)
+        @user.topics.find(topic.id).destroy
+      end
+    end
     redirect_to user_mentor_path
   end
 end
