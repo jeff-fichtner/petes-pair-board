f5159782d3ffa3d6d6302789e1bd2308a4b4cbde
Jeff Fichtner <jff2501@gmail.com>
2016-10-07 07:58:18 -0500

    Show test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 describe UsersController do
+  describe "GET #show" do
+    it "responds with status code 302" do
+      get :show
+      expect(response).to have_http_status 302
+    end
 
+    it "redirects to the pairings homepage" do
+      get :show
+      expect(response).to redirect_to pairings_path
+    end
+  end
 end
