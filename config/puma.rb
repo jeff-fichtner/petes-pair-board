aed642a88ac5fd4a4ad878a69a4373106e788563
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-08 15:37:21 -0500

    Puma config thread count for safe threading

--- config/puma.rb
+++ config/puma.rb
@@ -3,18 +3,24 @@
 # create Procfile/run this config file - declares what commands are run by your app's dynos
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 4)
-#
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# how many workers per dyno (RAM)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 1)
+# how many threads per worker (CPU)
 threads threads_count, threads_count
+# best practice: set min equal to max threads_count
 
-preload_app!
+# preload_app!
+# reduces startup time of individual puma worker processes
 
 rackup      DefaultRackup
+# tells Puma how to start your app/points to config.ru
 port        ENV['PORT']     || 3000
+# port that Puma binds to - heroku sets this but locally it should be rails default 3000
 environment ENV['RACK_ENV'] || 'development'
+# sets environment - heroku sets this to 'production'
 
 on_worker_boot do
-  # worker specific setup
+  # control over connecting workers to db
   ActiveSupport.on_load(:active_record) do
     config = ActiveRecord::Base.configurations[Rails.env] ||
                 Rails.application.config.database_configuration[Rails.env]
