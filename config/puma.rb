1feff40815231dd7f90e5e4fd0baed9f3b06858b
Jeff Fichtner & Thuy <pair+jeff-fichtner.thuynt13@devbootcamp.com>
2016-10-08 15:17:45 -0500

    New puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,9 @@
+# include gem in Gemfile
+
+# create Procfile/run this config file - declares what commands are run by your app's dynos
+
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+#
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
@@ -9,7 +14,11 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
